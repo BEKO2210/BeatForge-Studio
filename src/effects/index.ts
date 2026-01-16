01c7f2d36b01c30ccf7f5cb8abe31ce7c47e0ef0
Belkis Aslani <belkis.aslani@gmail.com>
2026-01-16 02:04:47 +0100

    revert(09-02): remove particle system per user request

--- src/effects/index.ts
+++ src/effects/index.ts
@@ -1,7 +1,6 @@
 // Effects system - post-processing effects for visualizers
 export { getCameraShakeOffset } from './cameraShake';
 export { renderVignette } from './vignette';
-export { ParticleRenderer } from './ParticleRenderer';
 export type {
   CameraShakeConfig,
   VignetteConfig,
@@ -13,8 +12,3 @@ export {
   DEFAULT_VIGNETTE,
   DEFAULT_EFFECTS,
 } from './types';
-
-// Particle system exports
-export { ParticleSystem, Particle } from './particles';
-export type { ParticleConfig, ParticleState } from './particles';
-export { DEFAULT_PARTICLE_CONFIG, DEFAULT_PARTICLE_COLORS } from './particles';
--- src/effects/particles/index.ts
+++
@@ -1,5 +0,0 @@
-// Particle system exports
-export { Particle } from './Particle';
-export { ParticleSystem } from './ParticleSystem';
-export type { ParticleConfig, ParticleState } from './types';
-export { DEFAULT_PARTICLE_CONFIG, DEFAULT_PARTICLE_COLORS } from './types';
